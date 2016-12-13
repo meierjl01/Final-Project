7f338e34de3137bd6c57247b775a71bec199f2df
Jennifer Meier <meierjl01@gmail.com>
2016-12-13 14:50:07 -0600

    added footer with links to gihub and linked in accounts

--- app/scripts/components/footer.js
+++ app/scripts/components/footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 export default React.createClass({
   render() {
     return (
-      <footer>
+      <footer className="footer-container">
+        <div className="footer-div">&copy;Jennifer Meier 2016</div>
+        <nav className="footer-nav">
+          <Link to = "https://www.linkedin.com/in/meierjl01"><i className="fa fa-linkedin" aria-hidden="true"></i></Link>
+          <Link to = "https://github.com/meierjl01"><i className="fa fa-github" aria-hidden="true"></i></Link>
+        </nav>
       </footer>
     )
   }
